7670f4648521ae69863a782c81609b44a05d0e48
kuroax <kuroax08@hotmail.com>
2025-02-13 16:05:52 -0600

    commit

---
+++ app/blog/first/page.tsx
@@ -0,0 +1,8 @@
+
+
+
+
+export default function Blog() {
+    return <h1>First entry</h1>;
+  }
+  
---
+++ app/blog/second/page.tsx
@@ -0,0 +1,8 @@
+
+
+
+
+export default function Blog() {
+    return <h1>Second entry</h1>;
+  }
+  
